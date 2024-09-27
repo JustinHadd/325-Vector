4b661941a8e4b92ce4de4e7303c592c4c745717e
Justin Haddock <jhaddoc5@ut.utm.edu>
2024-09-27 13:22:11 -0500

    Added doqmnt-style documentation

--- Vector.h
+++ Vector.h
@@ -1,7 +1,24 @@
+/**
+ * @file Vector.h
+ * @author Justin Haddock
+ * @date 2024-09-27
+ * @brief Vector class header file
+ * 
+ * This header file contains the definition of the Vector class as used in this program
+ */
+
 #ifndef VECTOR_H
 #define VECTOR_H
 #include <iostream>
 
+
+/**
+ * The Vector class allows for a vector object to be created, that has dynamic memory.
+ *
+ * @class Vector Vector.h "325-Vector/Vector.h"
+ * @brief An array with dynamic memory
+ *
+ */
 class Vector {
 private:
   int *vec_ptr; //Points to the vector's dynamic array
